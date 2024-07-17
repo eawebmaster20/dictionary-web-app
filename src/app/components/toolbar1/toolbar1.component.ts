1a47a5c17f4b6efd4dbe2773d2ae367b576a1826
eawebmaster20 <eaweb20@gmail.com>
2024-07-17 16:31:48 +0000

    feat: added template and template styles to the component

--- src/app/components/toolbar1/toolbar1.component.ts
+++ src/app/components/toolbar1/toolbar1.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ThemeTogglerComponent } from './theme-toggler/theme-toggler.component';
 
 @Component({
   selector: 'app-toolbar1',
   standalone: true,
-  imports: [],
+  imports: [ThemeTogglerComponent],
   templateUrl: './toolbar1.component.html',
   styleUrl: './toolbar1.component.scss'
 })
